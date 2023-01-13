739be7a3757b3028f260b053b44da6eb2140e8a3
wesleywil <wesleywilsonti@gmail.com>
2023-01-13 16:57:58 -0300

    added admin page to app navigation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,7 @@ import Homepage from "./pages/homepage/homepage";
 import Recipes from "./pages/recipes/recipes";
 
 import "./App.css";
+import Main from "./admin/pages/main/main";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Homepage />} />
           <Route path="/recipes" element={<Recipes />} />
+          <Route path="/admin" element={<Main />} />
         </Routes>
       </BrowserRouter>
     </div>
