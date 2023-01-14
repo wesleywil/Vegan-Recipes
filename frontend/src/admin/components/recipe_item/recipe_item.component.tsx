773e8d02c79bd525e047e9acc242962e2139a9b5
wesleywil <wesleywilsonti@gmail.com>
2023-01-14 13:20:52 -0300

    updated recipe item component to include the data type that will receive from database and added the fetchrecipes to the admin main page

--- frontend/src/admin/components/recipe_item/recipe_item.component.tsx
+++ frontend/src/admin/components/recipe_item/recipe_item.component.tsx
@@ -1,17 +1,29 @@
-const RecipeItem = () => {
+type RecipeItemInfo = {
+  id: string;
+  name: string;
+  category: string;
+  preparation_time: number;
+  special: boolean;
+};
+
+const RecipeItem = ({ ...item }: RecipeItemInfo) => {
   return (
-    <div className="w-full flex p-4">
+    <div className="w-full flex p-1">
       <div className="w-full self-center px-2 flex justify-between  text-white bg-[#358546d8]/60 z-10 rounded-xl">
         <div className="flex flex-col gap-0 py-2">
-          <h1 className="text-2xl font-semibold">Recipe</h1>
+          <h1 className="text-2xl font-semibold">
+            {item.name} {item.special ? "- Special" : ""}
+          </h1>
 
-          <h3>Category name - 00m</h3>
+          <h3>
+            {item.category} - {item.preparation_time}m
+          </h3>
         </div>
         <div className="flex self-center gap-2 p-2">
-          <button className="flex-none h-12 px-2 border-2 border-yellow-500 hover:text-gray-300 font-bold text-white rounded-xl transform duration-700 ease-in-out">
+          <button className="flex-none h-8 px-2 bg-yellow-500 hover:bg-yellow-300 text-white hover:text-gray-600 font-bold  rounded-xl transform duration-700 ease-in-out">
             Edit
           </button>
-          <button className="flex-none h-12 px-2 border-2 border-red-500 hover:text-gray-300 font-bold text-white rounded-xl transform duration-700 ease-in-out">
+          <button className="flex-none h-8 px-2  bg-red-500 hover:bg-red-300 text-white hover:text-gray-600 font-bold  rounded-xl transform duration-700 ease-in-out">
             Del
           </button>
         </div>
--- frontend/src/admin/pages/main/main.tsx
+++ frontend/src/admin/pages/main/main.tsx
@@ -1,6 +1,22 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import type { RootState, AppDispatch } from "../../../redux/store";
+import { fetchRecipes } from "../../../redux/dish/dish";
+
 import RecipeItem from "../../components/recipe_item/recipe_item.component";
+import SearchBar from "../../components/search_bar/search_bar.component";
+import DeleteConfirmation from "../../components/delete_confirmation/delete_confirmation.component";
 
 const Main = () => {
+  const status = useSelector((state: RootState) => state.dish.status);
+  const recipes = useSelector((state: RootState) => state.dish.recipes);
+  const dispatch = useDispatch<AppDispatch>();
+
+  useEffect(() => {
+    if (status === "idle") {
+      dispatch(fetchRecipes());
+    }
+  }, [status, recipes]);
   return (
     <div className="h-full min-h-screen	w-full min-w-screen">
       <svg viewBox="0 0 450 150">
@@ -9,21 +25,24 @@ const Main = () => {
         </text>
       </svg>
       <div className="w-1/2  mx-auto flex gap-2 my-2 px-4 font-bold text-white text-xl">
-        <button className="flex-none bg-[#358546d8] px-2 py-0 rounded-full">
+        <button className="flex-none bg-[#358546] hover:bg-[#20522b]/70 px-2 py-0 rounded-full transform duration-700 ease-in-out">
           New recipe
         </button>
-        <div className="w-full">
-          <input
-            type="text"
-            className="w-2/3 px-2 py-1 bg-black/20  text-white rounded-l-xl"
-          />
-          <button className="bg-[#358546d8] px-2 py-1 rounded-r-xl">
-            search
-          </button>
-        </div>
+        <SearchBar />
       </div>
-      <div className="w-1/2 mx-auto flex flex-col items-center gap-2 border border-[#358546] rounded-xl">
-        <RecipeItem />
+
+      <DeleteConfirmation />
+      <div className="w-1/2 mx-auto flex flex-col items-center border border-[#358546] rounded-xl">
+        {recipes.map((item, i) => (
+          <RecipeItem
+            key={i}
+            id={item.id}
+            name={item.name}
+            category={item.category}
+            preparation_time={item.preparation_time}
+            special={item.special}
+          />
+        ))}
       </div>
     </div>
   );
