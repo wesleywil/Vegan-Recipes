042072e8491d4cb6e0ca309397ff0606f8d8ba2d
wesleywil <wesleywilsonti@gmail.com>
2023-01-20 18:02:50 -0300

    updated recipe_item component by changing button texts to icons and added new function to handle ingredients view switch

--- frontend/src/admin/components/recipe_item/recipe_item.component.tsx
+++ frontend/src/admin/components/recipe_item/recipe_item.component.tsx
@@ -3,8 +3,10 @@ import type { AppDispatch } from "../../../redux/store";
 import {
   switch_form_view,
   switch_delete_view,
+  switch_ingredients_view,
 } from "../../../redux/admin/admin";
 import { selectById } from "../../../redux/dish/dish";
+import { FaList, FaEdit, FaTrashAlt } from "react-icons/fa";
 
 type RecipeItemInfo = {
   id: string;
@@ -27,6 +29,11 @@ const RecipeItem = ({ ...item }: RecipeItemInfo) => {
     dispatch(selectById(item.id));
   };
 
+  const handleIngredients = () => {
+    dispatch(switch_ingredients_view());
+    dispatch(selectById(item.id));
+  };
+
   return (
     <div className="w-full flex p-1">
       <div className="w-full self-center px-2 flex justify-between  text-white bg-[#358546d8]/60 z-10 rounded-xl">
@@ -41,16 +48,22 @@ const RecipeItem = ({ ...item }: RecipeItemInfo) => {
         </div>
         <div className="flex self-center gap-2 p-2">
           <button
-            onClick={() => handleEdit()}
-            className="flex-none h-8 px-2 bg-yellow-500 hover:bg-yellow-300 text-white hover:text-gray-600 font-bold  rounded-xl transform duration-700 ease-in-out"
+            onClick={() => handleIngredients()}
+            className="flex-none h-8 px-2 bg-blue-500/60 hover:bg-blue-300 text-white hover:text-gray-600 font-bold  rounded-xl transform duration-700 ease-in-out"
           >
-            Edit
+            <FaList />
+          </button>
+          <button
+            onClick={() => handleEdit()}
+            className="flex-none h-8 px-2 bg-yellow-500/60 hover:bg-yellow-300 text-white hover:text-gray-600 font-bold  rounded-xl transform duration-700 ease-in-out"
+          >
+            <FaEdit />
           </button>
           <button
             onClick={() => handleDelete()}
-            className="flex-none h-8 px-2  bg-red-500 hover:bg-red-300 text-white hover:text-gray-600 font-bold  rounded-xl transform duration-700 ease-in-out"
+            className="flex-none h-8 px-2  bg-red-500/60 hover:bg-red-300 text-white hover:text-gray-600 font-bold  rounded-xl transform duration-700 ease-in-out"
           >
-            Del
+            <FaTrashAlt />
           </button>
         </div>
       </div>
